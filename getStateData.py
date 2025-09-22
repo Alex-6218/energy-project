28e6b763d9aa27f29a2ba50043f6f238452920f4
Alex-6218 <alex77874@me.com>
2025-09-21 23:42:20 -0500

    deleted useless styles.css and made hover effect more readable, fiddled with getStateData probably

--- getStateData.py
+++ getStateData.py
@@ -2,7 +2,7 @@ import requests
 import urllib.parse
 
 SPREADSHEET_ID = "15GhUos1SrxZrnPV0LgaLbL_YQuEAZ5hmYZlt_jmVU5E"
-RANGE = "Energy Project!A4:E10"
+RANGE = "Energy Project!A1:E7"
 API_KEY = "AIzaSyCqwqvQ3ilLao55lUxMGp-HLS1RUxX4x68"
 
 encoded_range = urllib.parse.quote(RANGE, safe="!")
