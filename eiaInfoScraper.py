16bec6cacf78c8a82826b2d42dc6ce35f7a2c960
Alex-6218 <alex77874@me.com>
2025-09-22 16:23:54 -0500

    fiddlin

--- eiaInfoScraper.py
+++ eiaInfoScraper.py
@@ -1,6 +1,21 @@
 import requests
 
-url = "https://api.eia.gov/v2/seds/data/?frequency=annual&data[0]=value&start=2019&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&key=mdVcdSaFPwv9cmBQqN6aBqNvWsrI5iobLXSBwYjA"
-response = requests.get(url)
+params = {
+    "frequency": "annual",
+    "data[0]": "value",
+    "start": "2019",
+    "end": "2023",
+    "offset": "0",
+    "length": "5000",
+    "api_key": "mdVcdSaFPwv9cmBQqN6aBqNvWsrI5iobLXSBwYjA"
+}
+url = "https://api.eia.gov/v2/total-energy/data/?frequency=monthly"
+response = requests.get(url+"?", params=params)
 data = response.json()
-print(data)
+if int(data["response"]["total"]) > 5000:
+    print("Warning: More than 5000 results, not all results may be shown.")
+parsed_data = data["response"]["data"]
+
+for entry in parsed_data:
+        if(entry['unit'] == "Trillion Btu"):
+          print("Year: " + entry["period"] + ", Meter Serial Number: " + entry["msn"] + ", Value: " + entry["value"] + " " + entry["unit"], end="\n")
